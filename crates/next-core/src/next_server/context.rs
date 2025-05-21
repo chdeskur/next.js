9a298e2aafa1e437d1d9b7fc1db129d4798afb1a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-05-21 18:20:00 +0200

    is_module_merging_enabled on context, not in dev

--- crates/next-core/src/next_server/context.rs
+++ crates/next-core/src/next_server/context.rs
@@ -1030,7 +1030,8 @@ pub async fn get_server_chunking_context_with_client_assets(
         SourceMapsType::None
     })
     .module_id_strategy(module_id_strategy)
-    .file_tracing(next_mode.is_production());
+    .file_tracing(next_mode.is_production())
+    .module_merging(next_mode.is_production());
 
     if next_mode.is_development() {
         builder = builder.use_file_source_map_uris();
@@ -1095,7 +1096,8 @@ pub async fn get_server_chunking_context(
         SourceMapsType::None
     })
     .module_id_strategy(module_id_strategy)
-    .file_tracing(next_mode.is_production());
+    .file_tracing(next_mode.is_production())
+    .module_merging(next_mode.is_production());
 
     if next_mode.is_development() {
         builder = builder.use_file_source_map_uris()
