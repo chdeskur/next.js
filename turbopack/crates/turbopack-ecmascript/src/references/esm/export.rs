73d78157263620680ae725888c711f61a5b6e3f1
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-06-03 15:29:56 +0200

    Refactor ScopeHoistingContext

--- turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
@@ -557,11 +557,11 @@ impl EsmExports {
     pub async fn code_generation(
         self: Vc<Self>,
         chunking_context: Vc<Box<dyn ChunkingContext>>,
-        scope_hoisting_context: Option<ScopeHoistingContext<'_>>,
+        scope_hoisting_context: ScopeHoistingContext<'_>,
         parsed: Option<Vc<ParseResult>>,
         export_usage_info: Option<ResolvedVc<ModuleExportUsageInfo>>,
     ) -> Result<CodeGeneration> {
-        if scope_hoisting_context.is_some_and(|ctx| !*ctx.modules.get(&ctx.module).unwrap()) {
+        if scope_hoisting_context.skip_module_exports() {
             // If the current module is not exposed, no need to generate exports
             return Ok(CodeGeneration::empty());
         }
@@ -575,15 +575,10 @@ impl EsmExports {
 
         let mut dynamic_exports = Vec::<Box<Expr>>::new();
         {
-            let id = if let Some(scope_hoisting_context) = scope_hoisting_context
+            let id = if let Some(module) = scope_hoisting_context.module()
                 && !expanded.dynamic_exports.is_empty()
             {
-                Some(
-                    scope_hoisting_context
-                        .module
-                        .chunk_item_id(Vc::upcast(chunking_context))
-                        .await?,
-                )
+                Some(module.chunk_item_id(Vc::upcast(chunking_context)).await?)
             } else {
                 None
             };
@@ -721,11 +716,8 @@ impl EsmExports {
 
         let early_hoisted_stmts = vec![CodeGenerationHoistedStmt::new(
             "__turbopack_esm__".into(),
-            if let Some(scope_hoisting_context) = scope_hoisting_context {
-                let id = scope_hoisting_context
-                    .module
-                    .chunk_item_id(Vc::upcast(chunking_context))
-                    .await?;
+            if let Some(module) = scope_hoisting_context.module() {
+                let id = module.chunk_item_id(Vc::upcast(chunking_context)).await?;
                 quote!("$turbopack_esm($getters, $id);" as Stmt,
                     turbopack_esm: Expr = TURBOPACK_ESM.into(),
                     getters: Expr = getters,
